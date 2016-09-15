4c1cdedc411cab57372ef68aa19cc41b9ff909ae
16111201 <matheusmorcinek@gmail.com>
2016-09-15 17:56:37 -0300

    ListArrayOfInteger - Updated

--- Algoritmos-E-Estruturas-De-Dados/src/App.java
+++ Algoritmos-E-Estruturas-De-Dados/src/App.java
@@ -2,38 +2,37 @@
 public class App {
 
 	public static void main(String[] args) {
-		
-		
-		ListArrayOfInteger lista = new ListArrayOfInteger(5);
-		
-		System.out.println("Tamanho da lista" + lista.size());
-		
-		
-		System.out.println("\nPreenche lista com 10 elementos: ");
 
-		for(int x = 0; x <=100; x++){
+		ListArrayOfInteger lista = new ListArrayOfInteger(5);
+		lista.listaTodos();
+
+		System.out.println("Tamanho da lista :" + lista.size());
+
+		System.out.println("\nPreenche lista com elementos: ");
+
+		for (int x = 0; x <= 5; x++) {
 			lista.add(x);
 		}
-	
-		System.out.println("\nLista todos elementos: ");
+
+
 		
-		
-		System.out.print("\nLista - { ");
-		for(int z = 0; z <=10; z++){
-			
-			if(z < 10){
-				System.out.print(z +", ");
-					
-			} else {
-				System.out.print(z +" ");
-				
-			}
-			
-		}
-		System.out.println("}");
-		
-		
-		
+		  System.out.println("\nLista todos elementos: "); lista.listaTodos();
+		  System.out.println("Tamanho da lista :" + lista.size());
+		  
+		  lista.teste();
+		  
+		  lista.add(0,17); 
+		  
+		  lista.listaTodos();
+		 
+		  System.out.println("Tamanho da lista :" + lista.size());
+		  
+		  
+		  System.out.println(lista.get(6));
+		  //arrumar retorno maior que count atual
+		  
+		  System.out.println(lista.isEmpty());
+		  
 		
 	}
 
--- Algoritmos-E-Estruturas-De-Dados/src/ListArrayOfInteger.java
+++ Algoritmos-E-Estruturas-De-Dados/src/ListArrayOfInteger.java
@@ -4,26 +4,27 @@ public class ListArrayOfInteger {
 	private int[] data;
 	private int count;
 
-	
-
 	public ListArrayOfInteger(int size) {
 		this.data = new int[size];
 		this.count = 0;
+		}
+	
+	public void teste(){
+		System.out.println("lenght "+data.length);
+		System.out.println("count "+count );
 	}
 	
 			/*
-			 * // 	Métodos
-					ListArrayOfInteger (int size)
-					void add (int element)
-					void add (int index , int element)
-					int get (int index)
+			 * // 	falta :
+					
 					int set (int index , int element)
+					
 					boolean remove (int element)
 					int removeByIndex (int index )
-					boolean isEmpty ( )
+					
 					
 					boolean contains (int element)
-					int indexOf (int element)
+					
 					void clear ( ) 
 			 * */
 		
@@ -33,7 +34,7 @@ public class ListArrayOfInteger {
 	
 	private void increasesData(){
 		
-		int[] aux = new int[data.length];
+		int[] aux = new int[data.length+10];
 		
 		for(int x = 0; x < data.length; x++){
 			
@@ -55,33 +56,80 @@ public class ListArrayOfInteger {
 		}
 	}
 	
-	public void add(int index, int element){
-		
-		if(index < 0 || index > count){
-			//erro!
-		} else {
-		
-		if(count == data.length){
-			increasesData();
-		} else{
-			
-			for(int i = count; i <= index; i--){
-				data[i] = data[i-1];
-				listaTodos();
-			}
-			data[index] = element;
-			count++;
-		}
-		
-	}
-	
-	}
-	
+	public void add(int index, int element) {
 
-	public void listaTodos(){
-		for(int x = 0 ; x < data.length; x++){
-			System.out.print(x + ",");
+		if (index < 0 || index >= count) {
+			// erro!
+			System.out.println("erro index");
+		} else {
+
+			if (count == data.length) {
+				increasesData();
+			} else {
+
+				for (int i = count; i > index; i--) {
+					data[i] = data[i - 1];
+
+				}
+				data[index] = element;
+				count++;
+			}
+
+		}
+
+	}
+
+	public void listaTodos() {
+
+		if (count == 0) {
+			System.out.println("lista vazia");
+		} else {
+			System.out.print("\n{ ");
+			for (int z = 0; z < count; z++) {
+
+				if (z < count-1) {
+					System.out.print(data[z] + ", ");
+
+				} else {
+					System.out.print(data[z] + " ");
+
+				}
+
+			}
+			System.out.println("}");
 		}
 	}
+	
+	public int get(int index){
+		
+		if(index < 0 || index >= count){
+			//erro
+			System.out.println("erro");
+			
+		} 
+		
+		return data[index];
+		
+	}
+	
+	public boolean isEmpty(){
+		return (count == 0);
+	}
+	
+	
+	public int indexOf(int element){
+		
+		for(int x = 0; x < count; x++){
+			if(element == data[x]){
+				return x;
+			}
+		}
+		return -1;
+		
+	}
+	
+	
+	
+	
 
 }
